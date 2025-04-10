d0030aa86d551fde548c20851030c46ded75fd25
rexjmn <rexmayorga97@gmail.com>
2025-04-10 13:37:29 +0200

    CDCI2

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello World' });
-})
+export default function handler(req, res) {
+    res.status(200).json({ message: 'Hello World' });
+  }
 
 app.listen(3001, () => {
     console.log('Server is running on port 3001');
 })
+
